ba55159215d9b1eae0546dda6ee3365143c0f6cf
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-10-29 16:13:01 +0500

    fix log

--- src/middlewares/responce_time.js
+++ src/middlewares/responce_time.js
@@ -5,7 +5,8 @@ async function res_start_time(req, res, next) {
 
 async function res_end_time(req, res, next) {
     res.on("finish", ()=>{
-        console.log(`${req.originalUrl} | time: ${((new Date).getTime() - req.startTime)/1000}s`);      
+        console.log(req);
+        console.log(`[${req.method} ${req.originalUrl} ${JSON.stringify(req.body)} ${res.statusCode} ${((new Date).getTime() - req.startTime)/1000}s]`);      
     });
     next();
 }
