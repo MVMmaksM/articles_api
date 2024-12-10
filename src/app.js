f6cd24c223b68a590af417bba7104cd89aaa51b9
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-12-10 16:54:41 +0500

    add cors

--- src/app.js
+++ src/app.js
@@ -9,13 +9,17 @@ import error_handler from "./middlewares/errors_handler.js";
 import route_not_found from "./middlewares/route_not_found.js";
 import article_tags_router from "./routes/article_tags/article_tags.routes.js";
 import {res_start_time, res_end_time} from "./middlewares/responce_time.js";
-import compression from 'compression';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(compression());
 
 const http_port = 5000;
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}));
 
 //устанавливаем время начала запроса
 app.use(res_start_time);
